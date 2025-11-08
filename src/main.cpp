2cadcad2c9a39606e72f95989b19a29719415fc1
mohammed-tahmid-sudo <mohammedtahmid.bd@gmail.com>
2025-11-08 22:31:07 +0600

    updates

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,5 @@
 #include "optimizers/Optimizer.h"
 
 int main() {
-    std::cout << "Torch Alt ML Library Test\n";
-
-    // Example stub usage
-    std::cout << "Creating a Tensor...\n";
-    // Tensor t({2, 3}); // Uncomment when Tensor class is implemented
-
-    std::cout << "Adding a Dense layer...\n";
-    // Dense d(3, 2); // Uncomment when Dense class is implemented
-
-    std::cout << "Applying ReLU activation...\n";
-    // ReLU relu; // Uncomment when Activation class is implemented
-
-    std::cout << "ML Library test finished!\n";
-    return 0;
+	std::cout << "everything is working greate" << std::endl; 
 }
-
